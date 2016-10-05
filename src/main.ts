5a7b1df4916f021f993bdf0af941b79a25169f96
Marcin <marcin.obiedz@gmail.com>
2016-10-05 23:30:04 +0200

    First mock tree

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import * as d3 from 'd3';
+
 let xhr: XMLHttpRequest = new XMLHttpRequest();
 let method: string = "POST";
 let url: string = "http://localhost:49607/api/exptree";
@@ -14,3 +16,7 @@ let a = 'john rambo';
 //let a = 'db.Customers.AsQueryable().Where(cus => cus.CustomerID > 5 && cus.FirstName.StartsWith("Kat")).Take(5).Select(cus => new { cus.EmailAddress })';
 
 xhr.send(JSON.stringify(a));
+
+d3.layout
+.tree()
+.size();
